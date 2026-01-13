be648410ebf3f1dbfb35d579f64f4ecf09dfbe83
Szymon Lis <szymonlis@yahoo.com>
2026-01-13 13:04:12 +0100

    Added Test ImGui and ImPlot Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,113 @@
 #include <iostream>
 
-int main()
+#include "imgui.h"
+#include "implot.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <GLFW/glfw3.h>
+
+static void glfw_error_callback(int error, const char* description)
 {
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+int main(int, char**)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    // Set GL+GLSL versions
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Ellipsometry Simulator by VulpoByte", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui and ImPlot context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // App State
+    bool showGraphWindow = false;
+    ImVec4 clearColor = ImVec4(0.22f, 0.22f, 0.22f, 1.00f);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Show main window
+        {
+            ImGui::Begin("Main");
+            ImGui::Text("This is main window!");
+            ImGui::Checkbox("Graph Window", &showGraphWindow);
+            ImGui::End();
+        }
+
+        // Show graph window
+        if (showGraphWindow)
+        {
+            float x[] = {10.f, 23.f, 45.f, 67.f, 89.f};
+            float y[] = {-34.f, -0.5f, 43.f, 100.f, 67.f};
+            int pointCount{5};
+
+            ImGui::Begin("Graph Window", &showGraphWindow);
+            ImGui::Text("This is graph window!");
+
+            if(ImPlot::BeginPlot("Graph"))
+            {
+                ImPlot::PlotScatter("Data", x, y, pointCount);
+                ImPlot::EndPlot();
+            }
+
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        int frameBufferWidth, frameBufferHeight;
+        glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
+        glViewport(0,0, frameBufferWidth, frameBufferHeight);
+        glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
+
 
